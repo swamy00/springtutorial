a10899c23e7310dd8a00052d2d30a01559e9d752
swamy <swamy.sankaran@gmail.com>
2022-08-02 19:17:45 -0700

    Initial Commit

---
+++ src/test/java/me/swamy/spring/SpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package me.swamy.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
